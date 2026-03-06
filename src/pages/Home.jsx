4c1ca1acba239baa44aa4b0f15a198b04ed1f374
sarahnymann <saj004@edu.zealand.dk>
2026-03-06 18:55:39 +0100

    Add Lemon emoji

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import LemonadeStand from "../components/LemonadeStand";
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to the Lemonade Stand!</h1>
+      <h1>Welcome to the Lemonade Stand!🍋</h1>
       <LemonadeStand />
     </div>
   );
