915ac2b15b0178f98d6c087f4f4ee50734d0ca48
sarahnymann <saj004@edu.zealand.dk>
2026-03-06 16:30:49 +0100

    Configure base path for routing

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,15 @@ import Cart from "./pages/Cart";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="lemonadeStand">
       <div>
         <h1>Welcome to the Lemonade Stand!</h1>
         <Navbar />
 
         <Routes>
-          <Route path="/lemonadeStand" element={<Home />} />
-          <Route path="/lemonadeStand/shop" element={<Shop />} />
-          <Route path="/lemonadeStand/cart" element={<Cart />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
       </div>
     </BrowserRouter>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav>
-      <Link to="/lemonadeStand">Home</Link> |{" "}
-      <Link to="/lemonadeStand/shop">Shop</Link> |{" "}
-      <Link to="/lemonadeStand/cart">Cart</Link>
+      <Link to="/">Home</Link> |{" "}
+      <Link to="/shop">Shop</Link> |{" "}
+      <Link to="/cart">Cart</Link>
     </nav>
   );
 }
